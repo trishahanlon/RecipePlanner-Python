2b7903a589693d0d9e17199ea78703d2f610fd04
Trisha Moyer <tehanlon@us.ibm.com>
2017-07-26 18:46:59 -0500

    Committing because i'm about to completely change this.

--- addRecipe.py
+++ addRecipe.py
@@ -16,31 +16,21 @@ class addARecipe(Frame):
         servings = StringVar()
         favoriteVar = IntVar()
 
-        recipeNameLabel = Label(self, text="Recipe Name", font=MEDIUM_FONT)
-        recipeNameLabel.grid(row=1, column=0)
-        recipeName = Entry(self, show="", textvariable=name)
-        recipeName.grid(row=1, column=1)
+        Label(self, text="Recipe Name", font=MEDIUM_FONT).grid(row=1, column=0)
+        Entry(self, show="", textvariable=name).grid(row=1, column=1)
 
-        timeLabel = Label(self, text="Cook time (in mins)", font=MEDIUM_FONT)
-        timeLabel.grid(row=2, column=0)
-        time = Entry(self, show="", textvariable=time)
-        time.grid(row=2, column=1)
+        Label(self, text="Cook time (in mins)", font=MEDIUM_FONT).grid(row=2, column=0)
+        Entry(self, show="", textvariable=time).grid(row=2, column=1)
 
-        numberOfServingsLabel = Label(self, text="Number of Servings", font=MEDIUM_FONT)
-        numberOfServingsLabel.grid(row=3, column=0)
-        numberOfServings = Entry(self, show="", textvariable=servings)
-        numberOfServings.grid(row=3, column=1)
+        Label(self, text="Number of Servings", font=MEDIUM_FONT).grid(row=3, column=0)
+        Entry(self, show="", textvariable=servings).grid(row=3, column=1)
 
-        favoriteLabel = Label(self, text="Add to your favorites?", font=MEDIUM_FONT)
-        favoriteLabel.grid(row=4, column=0)
-        favoriteValue = Checkbutton(self, text="Favorite", variable=favoriteVar)
-        favoriteValue.grid(row=4, column=1)
+        Label(self, text="Add to your favorites?", font=MEDIUM_FONT).grid(row=4, column=0)
+        Checkbutton(self, text="Favorite", variable=favoriteVar).grid(row=4, column=1)
 
-        ingredientsLabel = Label(self, text="Ingredients \n (quantity unit ingredient, i.e. 1 cup milk)", font=MEDIUM_FONT)
-        ingredientsLabel.grid(row=5, column=0)
+        Label(self, text="Ingredients \n (quantity unit ingredient, i.e. 1 cup milk)", font=MEDIUM_FONT).grid(row=5, column=0)
 
-        directionsLabel = Label(self, text="Directions", font=MEDIUM_FONT)
-        directionsLabel.grid(row=5, column=1)
+        Label(self, text="Directions", font=MEDIUM_FONT).grid(row=5, column=1)
 
         ingredients = Text(self, bg='#d3d3d3', width=30, height=20)
         ingredients.grid(row=6, column=0)
@@ -48,13 +38,11 @@ class addARecipe(Frame):
         directions = Text(self, bg='#d3d3d3', width=30, height=20)
         directions.grid(row=6, column=1)
 
-        submitButton = Button(self, text="Submit Recipe", command = lambda: submit(name.get(), time.get(), servings.get(), favoriteVar.get(),
-                                                                                      ingredients.get("1.0", END), directions.get("1.0", END)))
-        submitButton.grid(row=7, column=0)
+        Button(self, text="Submit Recipe", command = lambda: submit(name.get(), time.get(), servings.get(), favoriteVar.get(),
+                                                                                      ingredients.get("1.0", END), directions.get("1.0", END))).grid(row=7, column=0)
 
         from firstpage import firstPage
-        createButton = Button(self, text="Return Home", command=lambda: controller.show_frame(firstPage))
-        createButton.grid(row=8, column=0)
+        Button(self, text="Return Home", command=lambda: controller.show_frame(firstPage)).grid(row=8, column=0)
 
 def submit(name, time, servings, favorite, ingredients, directions):
     print("Name: ", name)
@@ -69,7 +57,6 @@ def submit(name, time, servings, favorite, ingredients, directions):
         intTime = int(time)
         try:
             intServings = int(servings)
-
             with sqlite3.connect(database_file) as conn:
                 # create the table if it hasn't been created yet
                 conn.execute('''CREATE TABLE IF NOT EXISTS recipe
--- mealPlan.py
+++ mealPlan.py
@@ -1,9 +1,9 @@
 from tkinter import *
-from calendary import Calendary
 import calendar
 import datetime
 from isoweek import Week
 from tkinter import ttk
+import sqlite3
 
 LARGE_FONT=("Verdana", 24)
 MEDIUM_FONT=("Verdana", 14)
@@ -13,7 +13,7 @@ NOW = datetime.datetime.now()
 class makeMealPlan(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        label = Label(self, text="Make Meal Page", font=LARGE_FONT).grid(row=0, column=0)
+        Label(self, text="Make Meal Page", font=LARGE_FONT).grid(row=0, column=0)
 
         dt = datetime.date(NOW.year, NOW.month, NOW.day)
         weekNumber = dt.isocalendar()[1]
@@ -30,18 +30,17 @@ class makeMealPlan(Frame):
         )
         cal_out.grid(row=1, column=0, columnspan=2)
 
-
         menu = Frame(self)
         menu.rowconfigure(0, weight=1)
         menu.columnconfigure(0, weight=1)
         menu.rowconfigure(1, weight=3)
         menu.columnconfigure(1, weight=3)
+        menu.grid(row=2, column=0)
 
         columnLabels = ["Breakfast", "Lunch", "Dinner"]
         for i in range(len(columnLabels)):
-            Label(menu, text = columnLabels[i], font = ("Verdana", 16), fg= "#d3d3d3").grid(row=1, column=i+2, pady= 10,
+            Label(menu, text = columnLabels[i], font = ("Verdana", 16), fg= "#d3d3d3").grid(row=0, column=i+2, pady= 10,
                                                                                         padx=85, sticky="nsew")
-
         mondayText = "Monday " + str(w.monday())
         tuesdayText = "Tuesday " + str(w.tuesday())
         wednesdayText = "Wednesday " + str(w.wednesday())
@@ -52,12 +51,62 @@ class makeMealPlan(Frame):
 
         labels = [mondayText, tuesdayText, wednesdayText, thursdayText, fridayText, saturdayText, sundayText]
         for i in range(len(labels)):
-            Label(menu, text = labels[i]).grid(row=i+2, column=0, padx = 5, pady=15, sticky="w")
+            Label(menu, text = labels[i]).grid(row=i+1, column=0, padx = 5, pady=15, sticky="w")
             sep = ttk.Separator(menu, orient="vertical")
-            sep.grid(row=i+2, column=1, padx=5, sticky="nsew")
+            sep.grid(row=i+1, column=1, padx=5, sticky="nsew")
 
-        menu.grid(row=2, column=0)
+        buttonDict = {}
+        for rows in range(len(labels)):
+            for columns in range(len(columnLabels)):
+                # buttons = Button(menu, text="Add meal to day", command=lambda x=rows+1, y=columns+2: addMeal(x, y))
+                buttons = Button(menu, text="Add meal to day", command=lambda x=rows + 1, y=columns + 2: addMeal(x, y))
+                buttons.grid(row=rows+1, column=columns+2)
+                buttons.position = (rows+1,columns+2)
+                buttonDict[buttons] = buttons.position
+
+        def addMeal(rowLocation, columnLocation):
+            print("add meal")
+            print(rowLocation, columnLocation)
+            menu.grid_forget()
+            addMealFrame = Frame(self)
+            addMealFrame.rowconfigure(0, weight=1)
+            addMealFrame.columnconfigure(0, weight=1)
+            addMealFrame.rowconfigure(1, weight=3)
+            addMealFrame.columnconfigure(1, weight=3)
+            addMealFrame.grid(row=2, column=0)
+
+            recipeNames = []
+
+            database_file = "meal_planner.db"
+            with sqlite3.connect(database_file) as conn:
+                cursor = conn.cursor()
+                selection = cursor.execute("""SELECT * FROM recipe""")
+                for result in [selection]:
+                    for row in result.fetchall():
+                        name = row[0]
+                        time = row[1]
+                        servings = row[2]
+                        favorite = row[3]
+                        ingredients = row[4]
+                        directions = row[5]
+                        print (name, time, servings, favorite, ingredients, directions)
+                        recipeNames.append(name)
+
+            print(str(len(recipeNames)))
+            for i in range(len(recipeNames)):
+                Button(addMealFrame, text=recipeNames[i], command=lambda:newFunction(recipeNames[i], addMealFrame,
+                                                                                     rowLocation, columnLocation)).grid(row=i, column=0)
+
+
+        def newFunction(recipe, view, row, column):
+            print("hi from new function")
+            print(recipe)
+            view.grid_forget()
+
+            recipeLabel = Label(menu, text=recipe)
+            recipeLabel.grid(row = row, column = column)
+            menu.grid()
+
 
         from firstpage import firstPage
-        createButton = Button(self, text="Return Home", command=lambda: controller.show_frame(firstPage))
-        createButton.grid(row=9, column=0)
+        Button(self, text="Return Home", command=lambda: controller.show_frame(firstPage)).grid(row=9, column=0)
